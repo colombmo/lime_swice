528f925dac641c87e359d26c85849451be5d8271
braveheart-star <mouse1996115@gmail.com>
2022-11-11 04:07:13 +0800

    style update for ckeditor

--- assets/packages/ckeditor/config.js
+++ assets/packages/ckeditor/config.js
@@ -2,6 +2,54 @@
  * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
+var editor;
+
+var plgnNames =
+    "limereplacementfields,codemirror,editorplaceholder,lsswitchtoolbars,sourcedialog";
+
+var plgnIconSize = "15px";
+var plgnIcons = [
+    "ri-fullscreen-fill",
+    "ri-code-s-slash-fill",
+    "ri-braces-fill",
+    "ri-grid-fill",
+    "ri-bold",
+    "ri-italic",
+    "ri-underline",
+     "ri-list-ordered",
+     "ri-list-check",
+     "ri-indent-increase",
+     "ri-indent-decrease",
+     "ri-align-left",
+     "ri-align-center",
+     "ri-align-right",
+     "ri-align-justify",
+     "ri-link",
+     "ri-link-unlink",
+     "ri-image-line"
+];
+
+var replacingIcons = [
+    "maximize",
+    "sourcedialog",
+    "createlimereplacementfields",
+    "switchtoolbar",
+    "bold",
+    "italic",
+    "underline",
+    "numberedlist",
+    "bulletedlist",
+    "outdent",
+    "indent",
+    "justifyleft",
+    "justifycenter",
+    "justifyright",
+    "justifyblock",
+    "link",
+    "unlink",
+    "image"
+];
+
 CKEDITOR.editorConfig = function (a) {
     
         a.plugins = "a11ychecker,dialogui,dialog,a11yhelp,about,xml,ajax,basicstyles,bidi,blockquote,notification,button,toolbar,clipboard,codemirror,panelbutton,panel,floatpanel,colorbutton,colordialog,menu,contextmenu,copyformatting,dialogadvtab,div,elementspath,enterkey,entities,popup,filebrowser,find,fakeobjects,floatingspace,listblock,richcombo,font,format,horizontalrule,htmlwriter,iframe,image,indent,indentblock,indentlist,justify,menubutton,language,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastefromword,pastetext,removeformat,resize,save,scayt,selectall,showblocks,showborders,emoji,sourcearea,specialchar,stylescombo,tab,table,tabletools,undo,videodetector,wsc,wysiwygarea,lineutils,widgetselection,widget,html5video,markdown";
@@ -77,5 +125,32 @@ CKEDITOR.editorConfig = function (a) {
         ];
         a.extraPlugins = "limereplacementfields,codemirror,editorplaceholder,lsswitchtoolbars,sourcedialog";
         a.removePlugins = 'sourcearea';
-        
-};
+}
+
+CKEDITOR.on("instanceReady", function (event) {
+    var this_instance = document.getElementById(event.editor.id + "_toolbox");
+
+    for (var i = 0; i < replacingIcons.length; i++) {
+        var this_button = this_instance.querySelector(
+            ".cke_button__" + replacingIcons[i] + "_icon"
+        );
+        console.log("this_button: ", this_button);
+        if (typeof this_button !== null) {
+            // if (typeof plgnIcons[i] === undefined)
+            //      icon = plgnDefault
+            // else
+            //      icon = plgnIcons[i];
+            this_button.setAttribute(
+                "style",
+                "background-image:none !important"
+            );
+
+            if (typeof this_button !== undefined) {
+                this_button.innerHTML =
+                    '<i class=" ' +
+                    plgnIcons[i] +
+                    '" style="cursor: default; font-size:15px; font-weight:600"></i>';
+            }
+        }
+    }
+});
