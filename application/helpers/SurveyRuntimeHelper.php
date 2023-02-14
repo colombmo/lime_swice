975de701808b24832c953326798705769354fdf9
Moreno Colombo <moreno.colombo@unifr.ch>
2023-02-14 12:03:37 +0100

    Updated final after_survey URL

--- application/helpers/SurveyRuntimeHelper.php
+++ application/helpers/SurveyRuntimeHelper.php
@@ -1315,7 +1315,7 @@ class SurveyRuntimeHelper
             if ($this->aSurveyInfo['aCompleted']['sSurveylsUrlDescription'] == "") {
                 $this->aSurveyInfo['aCompleted']['sSurveylsUrlDescription'] = $this->aSurveyInfo['surveyls_url'];
             }
-            $this->aSurveyInfo['surveyls_url'] = "http://127.0.0.1:8000/test?uid={UID}&experiment_id={EXPERIMENTID}&group_id={GROUPISMA}&next=" . urlencode($this->aSurveyInfo['surveyls_url']);
+            $this->aSurveyInfo['surveyls_url'] = "http://127.0.0.1:8000/after_survey?uid={UID}&experiment_id={EXPERIMENTID}&group_id={GROUPISMA}&next=" . urlencode($this->aSurveyInfo['surveyls_url']);
             // Replace expressions in the new url
             $this->aSurveyInfo['surveyls_url']               = passthruReplace($this->aSurveyInfo['surveyls_url'], $this->aSurveyInfo);
             $this->aSurveyInfo['surveyls_url']               = $this->processString($this->aSurveyInfo['surveyls_url'], 3, 1);
